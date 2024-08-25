77c48e7726af8671337f3170162b850ec92e33c5
shareef <shareefsk219@gmail.com>
2024-08-25 12:28:27 +0530

    hello

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors({
 app.use(express.json());
 
 
-const PORT = process.env.PORT || 3000;
+const PORT =  3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
